dba8ef2d9fe534393ec73946977c2e41d9339a81
qubixal <132064397+qubixal@users.noreply.github.com>
2026-07-03 12:17:12 +1000

    Initial Commit

---
+++ unisnap/unisnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  unisnapApp.swift
+//  unisnap
+//
+//  Created by Leo Li on 3/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct unisnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
